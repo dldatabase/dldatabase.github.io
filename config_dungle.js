1fd3d488c768f66a4a9fd9505af7470819218a28
dungle-graph <159420345+dungle-graph@users.noreply.github.com>
2024-08-07 21:05:09 +0700

    Update config_dungle.js

--- config_dungle.js
+++ config_dungle.js
@@ -55,15 +55,7 @@ function watermark(svg) {
     if ( watermark_image_url ) {
         wm.append("image")
             .attrs({x:-64, y:-64, width:128, height:128, "xlink:href":watermark_image_url});
-    }
     
-    if ( watermark_text ) {
-        wm.append("text")
-            .attrs({x:0, y:80, "font-size":28, "text-anchor":"middle", "class":"graph-name"})
-            .text(watermark_text);
-        wm.append("text")
-            .attrs({x:290, y:255, "font-size":18, "text-anchor":"middle", "class":"graph-name"})
-            .text(watermark_text2);	
     }
 	 // Extra flair
     svg.append("g")
